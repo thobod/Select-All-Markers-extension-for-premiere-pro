cec1df59d21b14aabfe80c1451ae2f6e50267c3a
Thomas <t.boddeus@gmail.com>
2024-08-21 19:20:14 +0200

    Now works on audio tracks too

--- com.example.premiereMarkerSelector/host/index.jsx
+++ com.example.premiereMarkerSelector/host/index.jsx
@@ -3,22 +3,8 @@ function selectAllMarkersInSelectedClip(colorIndex) {
         var sequence = app.project.activeSequence;
 
         if (sequence) {
-            var tracks = sequence.videoTracks;
-            for (var i = 0; i < tracks.numTracks; i++) {
-                var track = tracks[i];
-                for (var j = 0; j < track.clips.numItems; j++) {
-                    var clip = track.clips[j];
-                    if (clip.isSelected()) {
-						var clipItem = clip.projectItem;
-                        var markers = clipItem.getMarkers();
-                        var marker = markers.getFirstMarker();
-                        while (marker) {
-							manipulateMarker(marker, colorIndex);
-                            marker = markers.getNextMarker(marker);
-                        }
-                    }
-                }
-            }
+			manipulateAllMarkersOfTracks(sequence.videoTracks, colorIndex);
+			manipulateAllMarkersOfTracks(sequence.audioTracks, colorIndex);
 			return "Markers selected successfully!";
         }
         return "No selected clip found.";
@@ -27,6 +13,25 @@ function selectAllMarkersInSelectedClip(colorIndex) {
         return "Error: " + e.message;
     }
 }
+
+function manipulateAllMarkersOfTracks(tracks, colorIndex){
+	for (var i = 0; i < tracks.numTracks; i++) {
+        var track = tracks[i];
+        for (var j = 0; j < track.clips.numItems; j++) {
+            var clip = track.clips[j];
+            if (clip.isSelected()) {
+				var clipItem = clip.projectItem;
+                var markers = clipItem.getMarkers();
+				var marker = markers.getFirstMarker();
+                while (marker) {
+					manipulateMarker(marker, colorIndex);
+                    marker = markers.getNextMarker(marker);
+                }
+            }
+        }
+	}
+}
+	
 
 function manipulateMarker(marker, colorIndex){
 	marker.setColorByIndex(colorIndex);
